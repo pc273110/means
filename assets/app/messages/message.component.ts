642260dc63709428b0b4a36bf6b278ecd4a47868
pc273110 <30820743+pc273110@users.noreply.github.com>
2018-04-24 23:21:44 +0530

    upto 39

---
+++ assets/app/messages/message.component.ts
@@ -0,0 +1,26 @@
+import { Message } from './message.model';
+import { Component,Input } from '@angular/core';
+
+@Component({
+    selector:'app-message',
+    templateUrl:'./message.component.html',
+    styles:[`
+    .author{
+        display:inline-block;
+        font-style:italic;
+        font-size:12px;
+        width:80%;
+    }
+    .config{
+        display:inline-block;
+        text-align:right;
+        font-size:12px;
+        width:19%;
+    }
+    `]       
+})
+
+export class MessageComponent{
+    
+    @Input('inputMessage') message: Message;
+}
